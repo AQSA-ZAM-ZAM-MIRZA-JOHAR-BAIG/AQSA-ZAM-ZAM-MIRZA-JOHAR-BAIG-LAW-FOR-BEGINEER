4f1c8c5d3fd63a0dfd7c9cf53630f53882619cf6
ayush8 <ayushdayal900@gmail.com>
2026-04-23 01:05:27 +0530

    fixing errors

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://law-for-beginners.vercel.app',
+  siteUrl: process.env.SITE_URL || 'https://aqsazammam.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: 'public',
